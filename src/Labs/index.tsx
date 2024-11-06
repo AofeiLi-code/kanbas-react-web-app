66fcec68877d86ddc163359adb33b776dc5c95ae
AofeiLi-code <aofei.li2097@gmail.com>
2024-11-06 01:20:43 -0500

    a4 Redux

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -1,25 +1,29 @@
+import { Routes, Route, Navigate } from "react-router";
 import Lab1 from "./Lab1";
-import { Route, Routes, Navigate } from "react-router";
 import Lab2 from "./Lab2";
 import Lab3 from "./Lab3";
-import Lab4 from "./Lab4";
 import TOC from "./TOC";
+import Lab4 from "./Lab4";
+import { Provider } from "react-redux";
+import store from "./store";
 export default function Labs() {
   return (
-    <div>
-      <h1>Labs</h1>
-      <h3>Aofei Li</h3>
-      <TOC />
+    <Provider store={store}>
+      <div id="wd-labs">
+        <h1>Labs</h1>
+        <h3>Aofei Li</h3>
+        <TOC />
       <a href="https://github.com/AofeiLi-code/kanbas-react-web-app.git" id="wd-github">
         My GitHub Repository
       </a>
-      <Routes>
-        <Route path="/" element={<Navigate to="Lab1" />} />
-        <Route path="Lab1" element={<Lab1 />} />
-        <Route path="Lab2" element={<Lab2 />} />
-        <Route path="Lab3/*" element={<Lab3 />} />
-        <Route path="Lab4/*" element={<Lab4 />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Navigate to="Lab1" />} />
+          <Route path="Lab1" element={<Lab1 />} />
+          <Route path="Lab2" element={<Lab2 />} />
+          <Route path="Lab3/*" element={<Lab3 />} />
+          <Route path="Lab4/*" element={<Lab4 />} />
+        </Routes>
+      </div>
+    </Provider>
   );
 }
